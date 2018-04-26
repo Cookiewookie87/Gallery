f95a5e820040b2d3a76197efd0e93c2d7122cce3
Cookiewookie87 <jernej.tratnik@gmail.com>
2018-04-26 14:28:12 +0200

    Added functionality

--- js/script.js
+++ js/script.js
@@ -8,17 +8,32 @@ function htmlGenerator ([v, h]) {
     <div class="item h${v} v${h}">
         <img src="img/${randomNumber(12)}.jpg">
         <div class="item-overlay">
-            <button>View</button>
+            <button id="view">View</button>
         </div>
     </div>
     `;
 }
-
 function randomNumber (max) {
     return Math.floor((Math.random() * max) + 1);;
 }
+function buttonClick (e) {
+    const pictureSrc = e.currentTarget.querySelector('img').src;
+    overlayImage.src = pictureSrc;
+    overlay.classList.add("open");
+
+}
+function closeImage () {
+    overlay.classList.remove("open");
+}
+
 
 let arrGenerator = Array.from({ length: 50 }, () => [randomNumber(4), randomNumber(4)]).concat([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]);
 
 let htmlConstructor = arrGenerator.map(htmlGenerator).join("");
 gallery.innerHTML = htmlConstructor;
+
+const items = document.querySelectorAll('.item');
+
+items.forEach(item => item.addEventListener("click", buttonClick));
+
+overlayClose.addEventListener("click", closeImage);
